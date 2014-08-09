7bec2cf311e895033525c22bb710c1a00b940304
yukaritan <yukaritan@users.noreply.github.com>
2014-08-09 19:15:24 +0200

    added a little bit of error handling to my cleverbot class

--- cleverbot_test.py
+++ cleverbot_test.py
@@ -1,3 +1,4 @@
+from time import sleep
 import cleverbot
 import omegle
 
@@ -8,7 +9,12 @@ class CleverbotOmegleBridge(omegle.OmegleAPI):
         self._cleverbot = cleverbot.Cleverbot()
 
     def handle_message(self, message: str) -> str:
-        return self._cleverbot.ask(message)
+        while True:
+            try:
+                return self._cleverbot.ask(message)
+            except cleverbot.CleverbotAPIRejection as ex:
+                print("Exception:", type(ex), ex)
+                sleep(1)
 
 
 if __name__ == '__main__':
